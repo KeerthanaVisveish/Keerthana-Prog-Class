bc73cffc264d270e4ef927a27bbe6151f62fc9ce
KeerthanaVisveish <keerthana.visveish@gmail.com>
2026-07-21 15:04:54 -0400

    followed instructions format oops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm/ArmHW.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm/ArmHW.java
@@ -6,25 +6,33 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class ArmHW {
-    private DcMotorEx armMotor;
-    private PIDController pidController;
+    private final DcMotorEx armMotor;
+    private final Telemetry telemetry;
+    private final PIDController pidController;
     private double kP = 0, kI = 0, kD = 0;
     private double kG = 0, ks = 0;
-    private int radsPerTick = 0;
+    private final int radsPerTick = 0;
+    private double targetAngle = 0;
 
     public ArmHW(HardwareMap hwMap, Telemetry telemetry) {
         armMotor = hwMap.get(DcMotorEx.class, "arm");
         pidController = new PIDController(kP, kI, kD);
+        this.telemetry = telemetry;
     }
 
     private double getCurrentArmPosition() {return armMotor.getCurrentPosition();}
 
-    private void setArmPosition(double targetAngle) { // angle in rads
+    private void update() {
         double currentAngle = getCurrentArmPosition() * radsPerTick;
         double feedForward = Math.signum(targetAngle - currentAngle) * ks;
         double feedBack = pidController.calculate(currentAngle, targetAngle);
         double gravityFeedForward = kG * Math.cos(currentAngle);
         double power = feedForward + feedBack + gravityFeedForward;
+        telemetry.addData("Arm Target Power: ", power);
         armMotor.setPower(power);
+    }
+
+    private void setTargetAngle(double desiredAngle) { // angle in rads
+        targetAngle = desiredAngle;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift/LiftHW.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift/LiftHW.java
@@ -1,28 +1,46 @@
 package org.firstinspires.ftc.teamcode.subsystems.Lift;
 
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.util.InterpLUT;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class LiftHW {
-    private DcMotorEx liftMotor;
-    private PIDController pidController;
-    private double kP = 0, kI = 0, kD = 0;
-    private double kG = 0, ks = 0;
+    private final DcMotorEx liftMotor;
+    private final Telemetry telemetry;
+    private final PIDController pidController;
+    private final double kP = 0, kI = 0, kD = 0;
+    private final double kG = 0, ks = 0;
+    private double targetPosition;
+    private InterpLUT kGLookup = new InterpLUT();
 
     public LiftHW(HardwareMap hwMap, Telemetry telemetry) {
         liftMotor = hwMap.get(DcMotorEx.class, "lift");
         pidController = new PIDController(kP, kI, kD);
+        this.telemetry = telemetry;
+        populateKGLookup();
+    }
+
+    private void populateKGLookup(){
+        kGLookup.add(0, 0);
+        kGLookup.add(50, 0);
+        kGLookup.add(100, 0);
+        kGLookup.add(150, 0);
     }
 
     private double getCurrentLiftPosition() {return liftMotor.getCurrentPosition();}
 
-    private void setLiftPosition(double targetPosition) {
+    private void update() {
         double currentPos = getCurrentLiftPosition();
         double feedForward = Math.signum(targetPosition - currentPos) * ks;
         double feedBack = pidController.calculate(currentPos, targetPosition);
-        double power = feedForward + feedBack + kG;
+        double gravityFeedForward = kGLookup.get(currentPos);
+        double power = feedForward + feedBack + gravityFeedForward;
+        telemetry.addData("Lift Target Power: ", power);
         liftMotor.setPower(power);
+    }
+    private void setTargetAngle(double desiredPosition) {
+        targetPosition = desiredPosition;
     }
 }
